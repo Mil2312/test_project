7c7f311580e6a0d7dbc536d34e1e4f48ebc10b04
Mil2312 <gulimlatypova6@gmail.com>
2025-05-10 23:02:02 +0600

    Update update_object.py

--- endpoints/update_object.py
+++ endpoints/update_object.py
@@ -1,23 +1,26 @@
 import requests
+from faker import Faker
 from endpoints.base_endpoint import Endpoint
+
 class UpdateObject(Endpoint):
     response = None
     response_json = None
 
-    def update_object(self, object_id, payload):
+    def update_object(self, object_id, payload=None):
+        fake = Faker()  # Инициализация Faker
+
         payload = {
-            "name": "Apple AirPods",
-            "data":
-                {
-                    "color": "white",
-                    "generation": "3rd",
-                    "price": 135
+                "name": fake.catch_phrase(),  # Случайное название (например, "Innovative wireless earbuds")
+                "data": {
+                    "color": fake.color_name(),  # Случайный цвет ("white", "black", "red" и т.д.)
+                    "generation": f"{fake.random_int(min=1, max=5)}th",  # "1st", "2nd", ... "5th"
+                    "price": fake.random_int(min=50, max=200)  # Случайная цена от 50 до 200
                 }
-        }
+            }
+
         requestUrl = f"https://api.restful-api.dev/objects/{object_id}"
         self.response = requests.put(requestUrl, json=payload)
         self.response_json = self.response.json()
-
 
     def check_response_name(self, name):
         assert self.response_json['name'] == name
